b53a574c8dfeec8d11ffa07c8add83178af08296
Takacs, Zsolt <zsolt.takacs.ext@siemens-energy.com>
2024-04-26 08:20:56 +0200

    Fix links

--- script.js
+++ script.js
@@ -35,36 +35,36 @@ const app = createApp({
 				   thumbnail: 'images/fish.jpg'
 			   },
 			   {
-                   title: 'barcodegenerator',
-                   link: 'https:github.com/ignotus87/barcodegenerator',
+                   title: 'barcode generator',
+                   link: 'https://github.com/ignotus87/barcodegenerator',
                    tags: ['.net', 'wpf', 'c#'],
                    description: 'a barcode generator implemented in wpf, utilizing zxing',
                    thumbnail: 'images/barcode.png'
                },
                {
                    title: 'laugh!',
-                   link: 'https:github.com/ignotus87/laughapp',
+                   link: 'https://github.com/ignotus87/laughapp',
                    tags: ['xamarin forms', 'xamarin android'],
                    description: 'a sample app showing a random image with playing a random sound, switching to another one when tapped. also includes an ad bar.',
                    thumbnail: 'images/laugh-thumbnail.png'
                },
                {
                    title: 'rfid reader',
-                   link: 'https:github.com/ignotus87/rfidreader',
+                   link: 'https://github.com/ignotus87/rfidreader',
                    tags: ['xamarin android'],
                    description: 'a xamarin app to collect mifare smart card ids or in fact any other rfid.',
                    thumbnail: 'images/smartcard.jpg'
                },
 			   {
                    title: 'wpf please wait - example application',
-                   link: 'https:github.com/ignotus87/wpfpleasewaitexample',
+                   link: 'https://github.com/ignotus87/wpfpleasewaitexample',
                    tags: ['.net', 'wpf', 'c#'],
                    description: 'a sample app demonstrating the use of the busyindicator of xceed wpf toolkit in an application.',
                    thumbnail: 'images/please-wait.jpg'
                },
 			   {
                    title: 'wpf printable user control template demo',
-                   link: 'https:github.com/ignotus87/wpfprintusercontrol',
+                   link: 'https://github.com/ignotus87/wpfprintusercontrol',
                    tags: ['.net', 'wpf', 'c#'],
                    description: 'a sample app showing you how to create a printable, a4-sized template of 2x6 labels containing some pieces of text.',
                    thumbnail: 'images/printable.jpg'
